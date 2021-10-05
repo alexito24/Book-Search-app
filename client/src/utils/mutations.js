244a2438d8a3d19b6e276ab2d78211640360a3ca
Heriberto lopez <alexito2002.ll@gmail.com>
2021-10-04 19:30:47 -0600

    fixed add_user in this file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $Password: String!) {
-    login(username: $username, email: $email, password: $password) {
+    user(username: $username, email: $email, password: $password) {
       token
       user {
         _id

43f3e20ea9a7a9cf6b471b7afa04b7630a4103c3
Heriberto lopez <alexito2002.ll@gmail.com>
2021-10-06 19:12:36 -0600

    trying to get heroku to work

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+
   useFindAndModify: false,
 });
 
